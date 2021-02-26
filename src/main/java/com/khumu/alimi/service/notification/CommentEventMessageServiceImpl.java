c81c826b76bd90f4b34422c2608f8e98f714239d
Jinsu Park <bo314@naver.com>
2021-02-26 16:51:24 +0900

    Fix: 새 댓글 작성자가 게시물 작성자 대신 알림 받는 버그

--- src/main/java/com/khumu/alimi/service/notification/CommentEventMessageServiceImpl.java
+++ src/main/java/com/khumu/alimi/service/notification/CommentEventMessageServiceImpl.java
@@ -1,10 +1,7 @@
 package com.khumu.alimi.service.notification;
 
 import com.google.gson.Gson;
-import com.khumu.alimi.data.Comment;
-import com.khumu.alimi.data.EventMessage;
-import com.khumu.alimi.data.Notification;
-import com.khumu.alimi.data.SimpleKhumuUser;
+import com.khumu.alimi.data.*;
 import com.khumu.alimi.repository.article.ArticleRepository;
 import com.khumu.alimi.repository.comment.CommentRepository;
 import com.khumu.alimi.repository.notification.NotificationRepository;
@@ -30,10 +27,19 @@ public class CommentEventMessageServiceImpl {
 
     public List<Notification> createNotifications(EventMessage<Comment> e) {
         Comment c = e.getResource();
+        c.setArticleObj(new Article(c.getArticleId()));
+
         List<Notification> results = new ArrayList<>();
         for (SimpleKhumuUser recipient : this.getRecipient(c)) {
+            Notification tmp = new Notification();
+            tmp.setRecipientObj(recipient);
+            tmp.setRecipient(recipient.getUsername());
+            tmp.setTitle("새로운 댓글이 생성되었습니다.");
+            tmp.setContent(c.getContent());
+            tmp.setKind("커뮤니티");
+
             Notification n = notificationRepository.create(
-                    new Notification(recipient.getUsername(), "댓글 생성돼써용~")
+                    tmp
             );
             System.out.println("Create: " + n);
             results.add(n);
@@ -47,15 +53,16 @@ public class CommentEventMessageServiceImpl {
         System.out.println(articleRepository.get(c.getArticleId()));
         String articleAuthorUsername = articleRepository.get(c.getArticleId()).getAuthorObj().getUsername();
         String newCommentAuthorUsername = c.getAuthorObj().getUsername();
+
         List<Comment> commentsInArticle = commentRepository.listFromArticle(c.getArticleObj().getId());
         List<SimpleKhumuUser> recipients = new ArrayList<>();
         // 게시물 작성자도 수신자. 단, 댓글 작성자가 게시물 작성자가 아닌 경우
-        if (!newCommentAuthorUsername.equals(articleAuthorUsername)) {
-            recipients.add(c.getAuthorObj());
+        if (!articleAuthorUsername.equals(newCommentAuthorUsername)) {
+            recipients.add(new SimpleKhumuUser(articleAuthorUsername));
         }
         for (Comment eachComment : commentsInArticle) {
-            // 새로운 댓글 작성자가 아니면서, 아직 수신자 목록에 없는 경우
-            if (!newCommentAuthorUsername.equals(eachComment.getAuthorObj().getUsername()) &&
+            // 새로운 댓글의 작성자가 아니면서, 아직 수신자 목록에 없는 경우
+            if (!eachComment.getAuthorObj().getUsername().equals(newCommentAuthorUsername) &&
                     recipients.stream().noneMatch(recipient -> recipient.getUsername().equals(eachComment.getAuthorObj().getUsername()))) {
                 recipients.add(eachComment.getAuthorObj());
             }
