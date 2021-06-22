6df5263a70aad2b187027689337f6239a8fe2d08
Jinsu Park <bo314@naver.com>
2021-06-22 21:45:49 +0900

    fix: jar 빌드 후 resources 찾지 못하는 문제

--- Dockerfile
+++ Dockerfile
@@ -3,20 +3,11 @@ WORKDIR /khumu
 COPY . .
 RUN ./gradlew build  --exclude-task test --exclude-task testClasses
 
+# jar에는 빌드 시의 resources가 포함될 수 있지만
+# 우리는 실행 시 외부에서 properties나 resource를 주입시켜줄 것임.
+# 오픈소스로 프로젝트를 관리하기 때문에 빌드 시 credential을 jar에 넣을 수 없음.
+# 그 외부 주입 경로는 build/resources/main/
+RUN cp -r src/main/resources/* build/resources/main
+RUN mv build/libs/*.jar build/libs/alimi.jar
 
-RUN cp build/libs/*.jar build/libs/alimi.jar
-# RUN cp build/libs/*.jar alimi.jar && \
-#    jar -xf alimi.jar
-# 단순히 jar만이 아니라 -xf로 jar을 풀어줘야
-# classpath를 올바르게 사용할 수 있더라 우선은 그 정도.
-# 근데 properties에서 include 말고 active profile을 이용하니까 괜찮아진 것 같기도........
-
-# ENTRYPOINT ["java", "-classpath", "BOOT-INF/classes:BOOT-INF/lib/*:/khumu/src/main/resources", "com.khumu.alimi.AlimiApplication"]
-# java -Dspring.profiles.active=dev -jar build/libs/alimi.jar
-
-# .jar의 경로를 기준으로가 아닌
-# ENTRYPOINT를 실행하는 path 기준으로 active profile을 찾음.
-WORKDIR /khumu/build/libs
-# .jar과 동일한 위치에 active profile을 위치시켜야함.
-# 추가적으로 firebase credential path도 수정해준 뒤 원하는 곳에 mount시켜줄 것
-ENTRYPOINT ["java", "-Dspring.profiles.active=dev", "-jar", "alimi.jar"]
+ENTRYPOINT ["java", "-Dspring.profiles.active=dev", "-jar", "build/libs/alimi.jar", "--spring.config.location=build/resources/main/"]
--- src/main/java/com/khumu/alimi/FireBaseConfig.java
+++ src/main/java/com/khumu/alimi/FireBaseConfig.java
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,18 +15,33 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class FireBaseConfig {
-    String FIREBASE_CREDENTIAL_FILE_PATH = "khumu-dev-firebase-credential.json";
+    @Value("${firebase.credential.pathType}")
+    String FIREBASE_CREDENTIAL_PATH_TYPE;
+    @Value("${firebase.credential.classPath}")
+    String FIREBASE_CREDENTIAL_CLASS_PATH;
+    @Value("${firebase.credential.absolutePath}")
+    String FIREBASE_CREDENTIAL_ABSOLUTE_PATH;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        System.out.println("AlimiApplication.firebaseApp");
-        ClassPathResource classPathResource = new ClassPathResource(FIREBASE_CREDENTIAL_FILE_PATH);
+        InputStream inputStream = null;
+        // jar 빌드 이후 따로 config file을 전달하려는 경우는 절대 경로를 이용해 file로 불러와야한다.
+        if (FIREBASE_CREDENTIAL_PATH_TYPE.equals("absolute")) {
+            log.info("FIREBASE_CREDENTIAL_PATH_TYPE: absolute");
+            inputStream = new FileInputStream(FIREBASE_CREDENTIAL_ABSOLUTE_PATH);
+        } else {
+            log.info("FIREBASE_CREDENTIAL_PATH_TYPE: classPath 로 처리");
+            ClassPathResource classPathResource = new ClassPathResource(FIREBASE_CREDENTIAL_CLASS_PATH);
+            inputStream = classPathResource.getInputStream();
+        }
+
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(classPathResource.getInputStream()))
-//			.setDatabaseUrl("")
+                .setCredentials(GoogleCredentials.fromStream(inputStream))
+//                .setDatabaseUrl("")
                 .build();
         FirebaseApp app = FirebaseApp.initializeApp(options);
         // 나중에 bean destroy 시에 app.delete를 해주는 게 좋긴 할 듯.
