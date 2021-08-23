c23d6e998f0ad95aac361ef0ca475dc5ae903c65
Jinsu Park <dev.umijs@gmail.com>
2021-08-23 20:53:25 +0900

    feat: put data to click

--- src/main/java/com/khumu/alimi/external/push/FcmPushManager.java
+++ src/main/java/com/khumu/alimi/external/push/FcmPushManager.java
@@ -4,6 +4,7 @@ import com.google.firebase.messaging.*;
 import com.khumu.alimi.data.entity.Notification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,8 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class FcmPushManager implements PushManager{
     final FirebaseMessaging firebaseMessaging;
+    @Value("${khumu.notification.rootLink}")
+    String NOTIFICATION_ROOT_LINK;
 
     public void notify(Notification n, String deviceToken) {
         try {
@@ -33,6 +36,7 @@ public class FcmPushManager implements PushManager{
                     .setTitle(n.getTitle())
                     .setBody(n.getContent())
                     .build())
+                .putData("link", NOTIFICATION_ROOT_LINK + "/" + n.getReference())
                 .build();
         return message;
     }
